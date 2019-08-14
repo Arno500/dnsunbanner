c47c6d5c2e4686ec09d2234a98e8af603330b4e4
Arno DUBOIS <arno.du@orange.fr>
2019-08-14 22:54:08 +0000

    Fixing IPv4 and IPv6 matching, to prevent setting false IPv6 DNS to IPv4 card

--- All_DNS_to_1.1.1.1.ps1
+++ All_DNS_to_1.1.1.1.ps1
@@ -5,14 +5,19 @@ Get-DnsClientServerAddress | % {
     $loopbackRegex = [regex]".*Loopback.*"
     $localRegex = [regex]".*local\*.*"
     if (($interfaceName -notmatch $bluetoothRegex) -and ($interfaceName -notmatch $teredoRegex) -and ($interfaceName -notmatch $loopbackRegex) -and ($interfaceName -notmatch $localRegex) ) {
-       echo "Setting DNS to CloudFlare for " + $interfaceName
-       Set-DnsClientServerAddress -InterfaceAlias $interfaceName -Validate -ServerAddresses "1.1.1.1,1.0.0.1,2606:4700:4700::1111,2606:4700:4700::1001"
+        if ($_.AddressFamily -eq "23") {
+            Set-DnsClientServerAddress -InterfaceAlias $interfaceName -Validate -ServerAddresses "2606:4700:4700::1111,2606:4700:4700::1001"
+            Write-Output "Setting DNS to CloudFlare for "$interfaceName" in "IPv6
+        }
+        elseif ($_.AddressFamily -eq "2") {
+            Set-DnsClientServerAddress -InterfaceAlias $interfaceName -Validate -ServerAddresses "1.1.1.1,1.0.0.1"
+            Write-Output "Setting DNS to CloudFlare for "$interfaceName" in "IPv4
+        }
     }
 }
 Pause
 
-function Pause ($Message="Press any key to continue...")
-{
-Write-Host -NoNewLine $Message;
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+function Pause ($Message = "Press any key to continue...") {
+    Write-Host -NoNewLine $Message;
+    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 }
